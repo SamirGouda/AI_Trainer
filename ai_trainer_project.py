01e95f9399981c399b6db846ecb8718e53237aa4
SamirGouda <samiir.ahmedd@gmail.com>
2021-08-05 13:38:04 +0200

    Initial commit

---
+++ ai_trainer_project.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+import pose_estimation_module as pm
+
+
+cap = cv2.VideoCapture('ai_trainer/curls2.mp4')
+detector = pm.PoseEstimation()
+
+prev_time = 0
+counts = 0
+direction = 0
+
+while cap.isOpened():
+    success, img = cap.read()
+    # img = cv2.imread('ai_trainer/2.jpg') # for testing
+    img = cv2.resize(img, (img.shape[1]//3, img.shape[0]//3))
+    img = detector.find_pose(img, False)
+    landmarks = detector.find_positions(img, False)
+    if len(landmarks) != 0:
+        # right arm
+        # detector.find_angle(img, (12, 14, 16)) # not visible in our test examples
+        # left arm
+        angle = detector.find_angle(img, [11, 13, 15])
+        percentage = np.interp(angle, (20, 160), (0, 100))
+        if angle >= 150 and direction == 0:
+            counts += 0.5
+            direction = 1
+        if angle <= 30 and direction == 1:
+            counts += 0.5
+            direction = 0
+        print(counts)
+        cv2.putText(img, f"counts: {int(counts)}", (40, 150), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+        cv2.rectangle(img, (40, 250), (70, 450), (0, 0, 255), 2)
+        cv2.rectangle(img, (70, 450), (40, 250+2*int(percentage)), (0, 0, 255), cv2.FILLED)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    cv2.putText(img, f"fps: {int(fps)}", (40, 50), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+    cv2.imshow('AI Trainer', img)
+
+    if cv2.waitKey(1) & 0XFF == ord('q'):
+        cap.release()
+        cv2.destroyAllWindows()
+        break
---
+++ pose_estimation_module.py
@@ -0,0 +1,86 @@
+import cv2
+import mediapipe as mp
+import time
+import math
+import numpy as np
+
+
+class PoseEstimation:
+    def __init__(self, mode=False, model=1, smooth_landmarks=True, detection_confidence=0.5, track_confidence=0.5):
+        self.mp_pose = mp.solutions.pose
+        self.pose = self.mp_pose.Pose(mode, model, smooth_landmarks, detection_confidence, track_confidence)
+        self.mp_draw = mp.solutions.drawing_utils
+        self.results = None
+        self.landmarks = []
+    def find_pose(self, img, draw=True):
+        img = cv2.resize(img, (1200,600))
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.pose.process(img_rgb)
+        # print(self.results.pose_landmarks)
+        if self.results.pose_landmarks:
+            if draw:
+                self.mp_draw.draw_landmarks(img, self.results.pose_landmarks, self.mp_pose.POSE_CONNECTIONS)
+        return img
+
+    def find_positions(self, img, draw=True):
+        self.landmarks = []
+        height, width, channels = img.shape
+        if self.results.pose_landmarks:
+            for lm_id, landmark in enumerate(self.results.pose_landmarks.landmark):
+                cx, cy = int(landmark.x * width), int(landmark.y * height)
+                self.landmarks.append([lm_id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 10, (255,0,0), cv2.FILLED)
+
+        return self.landmarks
+
+    def find_angle(self, img, points, draw=True):
+        # get the landmarks
+        x1, y1 = self.landmarks[points[0]][1:]
+        x2, y2 = self.landmarks[points[1]][1:]
+        x3, y3 = self.landmarks[points[2]][1:]
+        a = np.array([x1, y1])
+        b = np.array([x2, y2])
+        c = np.array([x3, y3])
+        # calculate the angle
+        ba = a - b
+        bc = c - b
+        cosine_angle = np.dot(ba, bc) / (np.linalg.norm(ba, 2) * np.linalg.norm(bc, 2))
+        angle_rad = np.arccos(cosine_angle)
+        angle = np.degrees(angle_rad)
+
+        if draw:
+            cv2.line(img, (x1,y1), (x2, y2), (255, 255, 255), 3)
+            cv2.line(img, (x3, y3), (x2, y2), (255, 255, 255), 3)
+            cv2.circle(img, (x1, y1), 10, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x1, y1), 15, (0, 0, 255), 2)
+            cv2.circle(img, (x2, y2), 10, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15, (0, 0, 255), 2)
+            cv2.circle(img, (x3, y3), 10, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x3, y3), 15, (0, 0, 255), 2)
+            cv2.putText(img, str(int(angle)), (x2+40, y2),
+                        cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+        return angle
+
+def main():
+    cap = cv2.VideoCapture('pose_videos/2.mp4')
+    prev_time = 0
+    while cap.isOpened():
+        ret, img = cap.read()
+        pose_detector = PoseEstimation()
+        img = pose_detector.find_pose(img)
+        #landmarks = pose_detector.find_positions(img)
+        #print(landmarks)
+        curr_time = time.time()
+        fps = 1 / (curr_time - prev_time)
+        prev_time = curr_time
+        cv2.putText(img, str(int(fps)), (70, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0), 3)
+        cv2.imshow('pose estimation', img)
+        if cv2.waitKey(1) & 0XFF == ord('q'):
+            cap.release()
+            cv2.destroyAllWindows()
+            break
+
+
+if __name__ == "__main__":
+    main()
